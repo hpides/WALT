2f6a504d58430b317b858ee733dc578a0910e6aa
Eric Ackermann <eric.ackermann@student.hpi.uni-potsdam.de>
2020-07-03 13:22:38 +0200

    Add license

--- deploy-docker-compose.sh
+++ deploy-docker-compose.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
+#
+# WALT - A realistic load generator for web applications.
+#
+# Copyright 2019-2020 Eric Ackermann <eric.ackermann@student.hpi.de>, Hendrik
+# Bomhardt <hendrik.bomhardt@student.hpi.de>, Benito Buchheim
+# <benito.buchheim@student.hpi.de>, Juergen Schlossbauer
+# <juergen.schlossbauer@student.hpi.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # no pushing to the docker registry
 unset deploy
 # make sure the output dir exists 
